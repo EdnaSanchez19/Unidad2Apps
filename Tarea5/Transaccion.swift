135d418202c7935d1d84ad7f66066bba78656624
EdnaSanchez19 <104112705+EdnaSanchez19@users.noreply.github.com>
2025-10-29 17:56:12 -0600

    Add files via upload

---
+++ Tarea5/Transaccion.swift
@@ -0,0 +1,20 @@
+//
+//  Transaccion.swift
+//  tarjetas_nav
+//
+//  Created by Edna Sanchez on 27/10/25.
+//
+
+import Foundation
+
+struct Transaccion : Identifiable {
+    
+    let id: Int
+    let uuid:UUID = UUID()
+    let image: String
+    let name: String
+    let subtitle: String
+    let date: String
+    let income: String
+    
+}
